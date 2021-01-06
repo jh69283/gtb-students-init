d7543ffbf501aba44387a3727d8bb2592ce8b151
Zhe Zhang <zhezhang@thoughtworks.com>
2021-01-06 16:00:48 +0800

    move First_Name.Family_Name to GitHub_Username

--- gtb-102-init.sh
+++ gtb-102-init.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
 THIS_TERM="102"
+STEP=0
 
 echo -e "\nHi, Welcome to GTB ${THIS_TERM}.\n"
 
-echo -e "1. Please tell me your Last Name (Surname Name), e.g. Zhao, Qian, Sun, Li, Zhou, Wu, Zheng, Wang:"
-read -r LAST_NAME
+######## GitHub username #######
+STEP=$((STEP + 1))
+echo -e "${STEP}. Please tell me your username on GitHub.com:"
 
-echo -e "2. And your First Name (Given Name) please, e.g. Xiangyun, Baochai, Xifeng, Tiezhu, Gousheng"
-read -r FIRST_NAME
+while :; do
+  read -r GITHUB_USERNAME
+  [[ $(curl -s -o /dev/null -w "%{http_code}" "https://api.github.com/users/${GITHUB_USERNAME}") -eq "200" ]] && break
+  echo -e "<< ERROR >>: We cannot verify this username with GitHub.com, Please try again or contact your coach:"
+done
+echo -e "\nGood job! Thank you ${GITHUB_USERNAME}.\n"
 
-STUDENT_NAME="${FIRST_NAME}.${LAST_NAME}"
-
-echo -e "\nGood job! Thank you ${STUDENT_NAME}.\n"
-echo -e "3. Your sonarqube token is: (please go to http://8.131.255.5/sonarqube/account/security/ to generate token if your don't have)"
+######## Sonarqube token #######
+STEP=$((STEP + 1))
+echo -e "${STEP}. Your sonarqube token is: (please go to http://8.131.255.5/sonarqube/account/security/ to generate one in case your don't have)"
 
 while :; do
   read -r SONARQUBE_TOKEN
   [[ ${SONARQUBE_TOKEN} =~ ^[a-z0-9]{40}$ ]] && break
   echo -e "<< ERROR >>: Your token should be 40 hexadecimals. Try again:"
 done
+
+######## Set up #######
 
 echo -e "\nYour local environment is setting up, please wait..."
 touch ~/.gradle/gradle.properties
@@ -29,8 +36,8 @@ grep -vq "systemProp.gtb" ~/.gradle/gradle.properties >tmp || mv tmp ~/.gradle/g
 {
   echo "systemProp.gtb.sonar.host.url=http://8.131.255.5/sonarqube/"
   echo "systemProp.gtb.sonar.login=${SONARQUBE_TOKEN}"
-  echo "systemProp.gtb.sonar.student.term=GTB.${THIS_TERM}"
-  echo "systemProp.gtb.sonar.student.name=${STUDENT_NAME}"
+  echo "systemProp.gtb.sonar.student.term=GTB-${THIS_TERM}"
+  echo "systemProp.gtb.sonar.student.name=${GITHUB_USERNAME}"
 } >>~/.gradle/gradle.properties
 
 echo -e "DONE."
