555f6f48d36aa2f2107b6c8d01e94ad5676e8256
Zhe Zhang <zhezhang@thoughtworks.com>
2021-01-17 05:17:59 +0800

    use gtb fullname instead of github username for sonar project keys

--- gtb-102-init.sh
+++ gtb-102-init.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
-THIS_TERM="102"
+THIS_TERM="term102"
 STEP=0
+BASE_URL="http://8.131.255.5/dashboard"
 
 echo -e "\nHi, Welcome to GTB ${THIS_TERM}.\n"
 
-######## GitHub username #######
+######## Family Name #######
 STEP=$((STEP + 1))
-echo -e "${STEP}. Please tell me your username on GitHub.com:"
+echo -e "${STEP}. Please tell me your Name"
 
 while :; do
-  read -r GITHUB_USERNAME
-  [[ $(curl -s -o /dev/null -w "%{http_code}" "https://api.github.com/users/${GITHUB_USERNAME}") -eq "200" ]] && break
-  echo -e "<< ERROR >>: We cannot verify this username with GitHub.com, Please try again or contact your coach:"
+  echo -e " > your Family Name (e.g. Zhao, Qian, Sun):"
+  read -r FAMILY_NAME
+
+  echo -e " > your Given Name (e.g. xiaoli, gousheng, xiaoshuan):"
+  read -r GIVEN_NAME
+
+  FULL_NAME=$(echo "${FAMILY_NAME}.${GIVEN_NAME}" | tr '[:upper:]' '[:lower:]')
+
+  [[ $(curl -s -o /dev/null -w "%{http_code}" "${BASE_URL}/api/terms/${THIS_TERM}/students?gtbUsername=${FULL_NAME}") -eq "200" ]] && break
+  echo -e "<< ERROR >>: We cannot verify your name, Please try again or contact your coach:"
 done
-echo -e "\nGood job! Thank you ${GITHUB_USERNAME}.\n"
+echo -e "\nGood job! Thank you ${FULL_NAME}.\n"
 
 ######## Sonarqube token #######
 STEP=$((STEP + 1))
-echo -e "${STEP}. Your sonarqube token is: (please go to http://8.131.255.5/sonarqube/account/security/ to generate one in case your don't have)"
+echo -e "${STEP}. Your sonarqube token is: (please go to ${BASE_URL}/sonarqube/account/security/ to generate one in case your don't have)"
 
 while :; do
   read -r SONARQUBE_TOKEN
-  [[ $(curl -s -o /dev/null -w "%{http_code}" -u "${SONARQUBE_TOKEN}": http://8.131.255.5/sonarqube/api/user_tokens/search) -eq "200" ]] && break
+  [[ $(curl -s -o /dev/null -w "%{http_code}" -u "${SONARQUBE_TOKEN}": ${BASE_URL}/sonarqube/api/user_tokens/search) -eq "200" ]] && break
   echo -e "<< ERROR >>: We cannot verify this username with GTB sonarqube server, Please try again or contact your coach:"
 done
 
@@ -36,8 +44,8 @@ grep -vq "systemProp.gtb" ~/.gradle/gradle.properties >tmp || mv tmp ~/.gradle/g
 {
   echo "systemProp.gtb.sonar.host.url=http://8.131.255.5/sonarqube/"
   echo "systemProp.gtb.sonar.login=${SONARQUBE_TOKEN}"
-  echo "systemProp.gtb.sonar.student.term=GTB-${THIS_TERM}"
-  echo "systemProp.gtb.sonar.student.name=${GITHUB_USERNAME}"
+  echo "systemProp.gtb.sonar.student.term=${THIS_TERM}"
+  echo "systemProp.gtb.sonar.student.name=${FULL_NAME}"
 } >>~/.gradle/gradle.properties
 
 echo -e "DONE."
